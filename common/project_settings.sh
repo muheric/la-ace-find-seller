1b54a08f570b469346266f305afb1fa39fe682d7
muheric <muheric2012@gmail.com>
2019-08-30 09:29:51 -0400

    Update of the project_settings.sh file

--- common/project_settings.sh
+++ common/project_settings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The name of the project to deploy services into.
-PROJECT_NAME="ace-demo-2"
+PROJECT_NAME="mygcpproject-250315"
 
 # Which default region should regional services use?
 PROJECT_REGION="us-central1"
@@ -17,42 +17,42 @@ PROJECT_ZONE="us-central1-b"
 ENV_TYPE="dev"
 
 # Used as a prefix for select service names.
-ORGANIZATION="find-seller"
+ORGANIZATION="muhebits.net"
 
 # This is the Storage bucket used for public assets. 
 # The entire bucket is public by default
-PUBLIC_ASSETS="fs2-public-bucket"
+PUBLIC_ASSETS="muheric-dev-public-bucket"
 
 # This is the Storage bucket used for private assets. 
 # The entire bucket is private by default
-PRIVATE_ASSETS="fs2-private-bucket"
+PRIVATE_ASSETS="muheric-dev-private-bucket"
 
 # The name of the Pubsub topic to create / use
-PUB_SUB_TOPIC="fs2-items"
+PUB_SUB_TOPIC="muheric-dev-items"
 
 # The name of the custom network for the product and ads services
-SERVICES_NETWORK="fs2-app-network"
+SERVICES_NETWORK="muheric-dev-app-network"
 
 # The name of the product service subnet
-PRODUCT_SUBNET="fs2-prod-app-network-subnet"
+PRODUCT_SUBNET="muheric-dev-prod-app-network-subnet"
 
 # The name of the product service subnet
-ADS_SUBNET="fs2-ad-app-network-subnet"
+ADS_SUBNET="muheric-dev-ad-app-network-subnet"
 
 # The name of the Kubernetes cluster used for the products service
-PRODUCT_CLUSTER_NAME="fs2-app-clusters"
+PRODUCT_CLUSTER_NAME="muheric-dev-app-clusters"
 
 # The name of the spanner instance
-PRODUCT_DB_INSTANCE_NAME="fs2-app-spanner-instance"
+PRODUCT_DB_INSTANCE_NAME="muheric-dev-app-spanner-instance"
 
 # The name of the spanner db
-PRODUCT_DB_NAME="fs2-app-spanner-db"
+PRODUCT_DB_NAME="muheric-dev-app-spanner-db"
 
 # The service account to use to launch the compute engine instances.
 COMPUTE_ENGINE_SERVICE_ACCOUNT=$(gcloud iam service-accounts list --filter='displayName:"Compute Engine default service account"' --format='value(email)')
 
 # The cloud function name
-FUNCTION_NAME="fs2-upload"
+FUNCTION_NAME="muheric-dev-upload"
 
 # Bigtable settings...
 # The name of our Bigtable instance. An instance is basically a container for our cluster.
